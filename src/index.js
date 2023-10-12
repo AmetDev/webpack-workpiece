40211468297ed3bc046bd731989054b365b467e0
AmetDev <ametsejdaliev@gmail.com>
2023-10-12 21:33:12 +0300

    react-sass-webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import '../styles/style.scss'
 import Hello from './Hello'
-
 const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(<Hello />)
